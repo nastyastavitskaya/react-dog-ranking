b824fcbb594d5c715001ddd7d1173444e2d3563b
Anastasiia Stavitskaya <stavitskaya.anastasiia@gmail.com>
2022-09-19 15:08:10 -0700

    Updates

--- src/styles/mixins.js
+++
@@ -1,3 +0,0 @@
-export const mediaQueryMobile = () => '@media (max-width: 767px)';
-export const mediaQueryTablet = () => '@media (max-width: 1024px)';
-export const mediaQueryTabletOrDesktop = () => '@media (min-width: 768px) and (max-width: 1280px)';
